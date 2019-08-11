1b5fc87a45e71e26d91f8a426a9187436a93f6f3
vozik <vozik@securcom.me>
2019-08-12 00:49:58 +0200

    react+graphviz

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ymString = `
+`;
+
+const dgString = `digraph G {
+  "Welcome" -> "To"
+  "To" -> "Web"
+  "To" -> "GraphViz!"
+}
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ymValue: ymString,
+      dgValue: dgString
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      ...this.state,
+      dgValue: event.target.value
+    });
+  };
+  handleYamlChange = (event) => {
+    this.setState({
+      ...this.state,
+      ymValue: event.target.value
+    });
+  };
+
+  render () {
+      return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello World!!!</p>
+          <textarea id="yaml_data" rows="5" cols="80" width="100%" wrap="off" value={this.state.ymValue}
+            onChange={this.handleYamlChange}
+          ></textarea>
+          <textarea id="graphviz_data" rows="5" cols="80" width="100%" wrap="off" value={this.state.dgValue}
+            onChange={this.handleChange}
+          >
+          </textarea>
+          <button id="generate_btn" disabled><b>Loading...</b></button>
+          <div id="graphviz_svg_div"></div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
